b739bbc7046802b13373ed58a840c2a968e7a649
Gal Cohen <galcohen08@gmail.com>
2018-05-05 17:23:54 +0200

    New Contact Model

--- WebAPI/src/main/java/com/web_api/WebApiApplication.java
+++ WebAPI/src/main/java/com/web_api/WebApiApplication.java
@@ -3,7 +3,7 @@ package com.web_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication(scanBasePackages={"com.web_api"})
 public class WebApiApplication {
 
 	public static void main(String[] args) {
---
+++ WebAPI/src/main/java/com/web_api/model/Contact.java
@@ -0,0 +1,5 @@
+package com.web_api.model;
+
+public class Contact {
+
+}
