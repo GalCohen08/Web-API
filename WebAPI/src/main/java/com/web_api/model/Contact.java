faaeea957b1f855b0e8e2289b921fb5107a4352d
Gal Cohen <galcohen08@gmail.com>
2018-05-05 17:32:30 +0200

    Creating simple table "Contact"

--- WebAPI/src/main/java/com/web_api/model/Contact.java
+++ WebAPI/src/main/java/com/web_api/model/Contact.java
@@ -1,5 +1,44 @@
 package com.web_api.model;
 
-public class Contact {
+import java.math.BigInteger;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contact")
+public class Contact {
+	@Id 
+	@GeneratedValue
+	@Column(name = "contact_id")
+	private Long id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String fullName;
+	
+	private String address;
+	
+	private String eMaill;
+	
+	private BigInteger  mobileNumber;
+	
+	public Contact() {
+		super();
+	}
+	
+	public Contact(String name, String lastName, String address, String eMaill, BigInteger mobileNumber, boolean active) {
+		super();
+		this.firstName = name;
+		this.lastName = lastName;
+		this.fullName = firstName+" "+lastName;
+		this.address = address;
+		this.eMaill = eMaill;
+		this.mobileNumber = mobileNumber;
+	}
 }
