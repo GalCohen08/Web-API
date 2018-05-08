e3ca8677e4fb528a0ecc6fb42e8e14a9b0b13c96
Gal Cohen <galcohen08@gmail.com>
2018-05-09 00:58:49 +0200

    Creating User Table

---
+++ WebAPI/src/main/java/com/web_api/model/User.java
@@ -0,0 +1,69 @@
+package com.web_api.model;
+
+import java.util.Collection;
+
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+@Table
+public class User implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	String id;
+	String username;
+	String password;
+	  
+	@JsonCreator
+	public User(String id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
